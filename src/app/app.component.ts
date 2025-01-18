c29576e3233ebb3fe8141cfb6c7c775ad5bdf346
Mohab ElHamzawy <melhamzawy.c@stc.com.sa>
2025-01-18 22:09:52 +0200

    CLEANUP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,26 +12,14 @@ import {HeaderComponent} from './core/components/header/header.component';
 export class AppComponent implements OnInit {
   // Injections
   #questionsService = inject(QuestionsService);
-  categoriesList: any[] = [];
-
-
-  getCategories(): void {
-    this.categoriesList = this.#questionsService.getCategories()
-  }
-
-  getQuestions(): void {
-    console.log(this.#questionsService.getQuestions());
-    // this.#questionsService.getQuestions().pipe(
-    //   tap((res: any) => {
-    //     this.questions = res;
-    //     console.log('FROM APP ', this.questions);
-    //   })
-    // )
-    //   .subscribe();
-  }
 
   ngOnInit(): void {
-    this.getQuestions();
+    this.#getQuestions();
     this.#questionsService.getTotalPoints();
   }
+
+  #getQuestions(): void {
+    this.#questionsService.getQuestions();
+  }
+
 }
